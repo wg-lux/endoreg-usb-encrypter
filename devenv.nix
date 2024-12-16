a8bb494f993b762eac84bf3af29536b3d09f4585
Thomas <tlux14@googlemail.com>
2024-12-16 09:18:54 +0100

    cleanup,migrate to devenv

---
+++ devenv.nix
@@ -0,0 +1,67 @@
+{ pkgs, lib, config, inputs, ... }:
+let
+  buildInputs = with pkgs; [
+    python311Full
+    # cudaPackages.cuda_cudart
+    # cudaPackages.cudnn
+    stdenv.cc.cc
+  ];
+
+  sops-nix = inputs.sops-nix;
+
+in 
+{
+
+  # A dotenv file was found, while dotenv integration is currently not enabled.
+  dotenv.enable = false;
+  dotenv.disableHint = true;
+
+
+  packages = with pkgs; [
+    cudaPackages.cuda_nvcc
+    stdenv.cc.cc
+    age
+    openvpn
+    openssh
+  ];
+  
+
+  env = {
+    LD_LIBRARY_PATH = "${
+      with pkgs;
+      lib.makeLibraryPath buildInputs
+    }:/run/opengl-driver/lib:/run/opengl-driver-32/lib";
+
+  };
+
+  languages.python = {
+    enable = true;
+    uv = {
+      enable = true;
+      sync.enable = true;
+    };
+  };
+
+  scripts.hello.exec = "${pkgs.uv}/bin/uv run python hello.py";
+  scripts.gpu-check.exec = "${pkgs.uv}/bin/uv run python gpu_check.py";
+  scripts.env-setup.exec = ''
+    export LD_LIBRARY_PATH="${
+      with pkgs;
+      lib.makeLibraryPath buildInputs
+    }:/run/opengl-driver/lib:/run/opengl-driver-32/lib"
+  '';
+
+
+  
+
+
+  tasks = {};
+
+  processes = {
+  };
+
+  enterShell = ''
+    . .devenv/state/venv/bin/activate
+    hello
+  '';
+}
